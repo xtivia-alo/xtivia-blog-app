b88bc45e48900d75e6bdfcc73eeda2a078e93f27
Anthony Lo <alo@xtivia.com>
2023-09-02 23:36:43 -0700

    some test entries to pull contentful data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { fetchLandingEntriesBySlug } from "@/lib/api";
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const entries = await fetchLandingEntriesBySlug(false);
-  console.log(params);
+  console.log(entries && entries[0].sys.contentType.sys.id);
+
+  console.log(entries && entries[0].fields.sections);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
