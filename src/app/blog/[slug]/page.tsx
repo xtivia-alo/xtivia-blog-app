f1a8ccb94014ea6090c2899559bfba08ff37741f
Anthony Lo <alo@xtivia.com>
2023-09-02 22:51:16 -0700

    initial api request set-up, page structure set up

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,11 +3,12 @@ import { fetchLandingEntriesBySlug } from "@/lib/api";
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const entries = await fetchLandingEntriesBySlug(false);
-  console.log(params);
+
+  console.log(params.slug);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+      Blog
     </main>
   );
 }
---
+++ src/lib/api.ts
@@ -0,0 +1,74 @@
+import { ContentfulClientApi } from "contentful";
+
+const space = process.env.CONTENTFUL_SPACE_ID;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+const previewToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+
+let client: ContentfulClientApi<undefined> | undefined;
+let previewClient: ContentfulClientApi<undefined> | undefined;
+
+if (accessToken) {
+  client = require("contentful").createClient({
+    space: space,
+    accessToken: accessToken,
+  });
+}
+
+if (previewToken) {
+  client = require("contentful").createClient({
+    space: space,
+    accessToken: previewToken,
+    host: "preview.contentful.com",
+  });
+}
+
+async function fetchEntries(
+  client: ContentfulClientApi<undefined> | undefined,
+  contentType: string,
+  slug: string
+) {
+  // if (accessToken) {
+  //   const entries = await client?.getEntries();
+  //   if (entries?.items) return entries.items;
+  // }
+
+  if (accessToken) {
+    const entries = await client?.getEntries({
+      content_type: contentType,
+      "fields.slug": slug,
+      include: 5,
+    });
+    if (entries?.items) return entries.items;
+    console.log(`Error getting Entries for ${contentType}.`);
+  }
+  console.log(`Access Token is undefined`);
+}
+
+async function fetchPreviewEntries(
+  client: ContentfulClientApi<undefined> | undefined,
+  contentType: string,
+  slug: string
+) {
+  if (accessToken) {
+    const entries = await client?.getEntries({
+      content_type: contentType,
+      "fields.slug": slug,
+      include: 5,
+    });
+    if (entries?.items) return entries.items;
+    console.log(`Error getting Entries for ${contentType}.`);
+  }
+  console.log(`Access Token is undefined`);
+}
+
+export async function fetchLandingEntriesBySlug(
+  preview: boolean,
+  slug: string = "home"
+) {
+  if (preview) {
+    console.log("Fetching Previews");
+    return fetchPreviewEntries(previewClient, "pageLanding", slug);
+  }
+  console.log("Fetching Published");
+  return await fetchEntries(client, "pageLanding", slug);
+}
